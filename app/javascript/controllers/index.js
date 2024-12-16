c01727ff3f739e81fcedb17230fdef222266f1c1
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-12-16 19:54:36 +0000

    Include stimulus controllers in index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,11 @@
 
 import { application } from "./application"
 
+import CloudflareTurnstileController from "./cloudflare_turnstile_controller"
+application.register("cloudflare-turnstile", CloudflareTurnstileController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import RubyUi__DismissAlertController from "./ruby_ui/dismiss_alert_controller"
+application.register("ruby-ui--dismiss-alert", RubyUi__DismissAlertController)
