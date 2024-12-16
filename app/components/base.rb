ddfa13275e7f8f8e641a62723d5bb9d9716f26ea
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-12-16 19:54:10 +0000

    Include rails helper methods in base component

--- app/components/base.rb
+++ app/components/base.rb
@@ -7,6 +7,8 @@ class Components::Base < Phlex::HTML
   include ::Devise::Controllers::UrlHelpers
   include Phlex::Rails::Helpers::CSPMetaTag
   include Phlex::Rails::Helpers::CSRFMetaTags
+  include Phlex::Rails::Helpers::Flash
+  include Phlex::Rails::Helpers::FormWith
   include Phlex::Rails::Helpers::JavascriptIncludeTag
   include Phlex::Rails::Helpers::Routes
   include Phlex::Rails::Helpers::StylesheetLinkTag
