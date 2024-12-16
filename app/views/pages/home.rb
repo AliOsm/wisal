0977cde5b70b76e9a2971794615b4d7c67b0e983
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-12-16 19:55:09 +0000

    Change home page top margin

--- app/views/pages/home.rb
+++ app/views/pages/home.rb
@@ -3,7 +3,7 @@ class Views::Pages::Home < Views::Base
 
   def view_template
     div(class: "flex flex-col items-center justify-center h-screen bg-[url('/home_bg.jpg')] bg-cover bg-center") do
-      div(class: "flex flex-col items-center justify-center space-y-2 mt-8") do
+      div(class: "flex flex-col items-center justify-center space-y-2 mt-10") do
         Link(href: new_report_path, variant: :primary, size: :xl, class: "flex items-center gap-3 w-full pe-5 me-3") do
           span { t("home.report_missing") }
 
