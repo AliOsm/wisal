38ccc24c0e77518e0d4a1d6ba319298b60555e17
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-12-16 19:58:54 +0000

    Implement new report form

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,9 @@
 class ReportsController < ApplicationController
+  before_action :set_report, only: :create
+  before_action :validate_cloudflare_turnstile, only: :create
+
+  rescue_from RailsCloudflareTurnstile::Forbidden, with: :cloudflare_turnstile_handler
+
   def new
     @report = Report.new
 
@@ -6,10 +11,8 @@ class ReportsController < ApplicationController
   end
 
   def create
-    @report = Report.new(report_params)
-
     if @report.save
-      redirect_to root_path, notice: t(".success")
+      redirect_back fallback_location: root_path, notice: t(".success")
     else
       render Views::Reports::New.new(report: @report), status: :unprocessable_entity
     end
@@ -19,5 +22,15 @@ class ReportsController < ApplicationController
 
   def report_params
     params.require(:report).permit(:name, :age, :last_known_place, :characteristics, :contact, :image)
+  end
+
+  def set_report
+    @report = Report.new(report_params)
+  end
+
+  def cloudflare_turnstile_handler
+    @report.errors.add(:base, t("reports.create.captcha_error_message"))
+
+    render Views::Reports::New.new(report: @report), status: :see_other
   end
 end
--- app/views/reports/new.rb
+++ app/views/reports/new.rb
@@ -3,7 +3,47 @@ class Views::Reports::New < Views::Base
     @report = report
   end
 
+  def title = t("reports.new.title")
+
   def view_template
-    # TODO
+    div(class: "flex flex-col items-center justify-center h-screen p-4 bg-[url('/bg.jpg')] bg-cover bg-top") do
+      Card(class: "w-full max-w-md max-sm:mt-20") do
+        CardHeader(class: "pb-4") do
+          CardTitle { t("reports.new.title") }
+        end
+
+        CardContent(class: "pb-2") do
+          form_with(model: @report, id: "report-form") do |form|
+            render Components::Shared::Errors.new(resource: @report)
+
+            div(class: "space-y-2") do
+              render Components::Forms::Field.new(form:, type: :text, name: :name, placeholder: Report.human_attribute_name(:name))
+              render Components::Forms::Field.new(form:, type: :number, name: :age, placeholder: Report.human_attribute_name(:age))
+              render Components::Forms::Field.new(form:, type: :text, name: :last_known_place, placeholder: Report.human_attribute_name(:last_known_place))
+              render Components::Forms::TextArea.new(form:, name: :characteristics, placeholder: Report.human_attribute_name(:characteristics))
+
+              div(class: "flex items-center justify-center shadow-sm rounded-md border overflow-hidden ps-3") do
+                span(class: "text-muted-foreground text-sm pe-2") { Report.human_attribute_name(:image) }
+                render Components::Forms::Field.new(form:, type: :file, name: :image, class: "border-0 rounded-none shadow-none ps-0 sm:ps-0.5 border-s")
+              end
+
+              render Components::Forms::Field.new(form:, type: :text, name: :contact, placeholder: Report.human_attribute_name(:contact))
+
+              div(data: { controller: "cloudflare-turnstile" }) do
+                cloudflare_turnstile
+              end
+            end
+          end
+        end
+
+        CardFooter(class: "flex justify-end gap-x-2") do
+          Button(variant: :primary, type: :submit, class: "flex items-center gap-2 pe-4", form: "report-form") do
+            span { t("reports.new.submit") }
+
+            Hero::PaperAirplane(variant: :solid, class: "size-5 scale-x-[-1]")
+          end
+        end
+      end
+    end
   end
 end
