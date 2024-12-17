bfb776e4ab8dd98a0055b0020a3db25ec89746be
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-12-17 20:28:47 +0000

    Fix rubocop warnings

--- app/components/forms/field.rb
+++ app/components/forms/field.rb
@@ -18,7 +18,7 @@ module Components
 
       def default_attrs
         {
-          class: 'flex h-9 w-full rounded-md border bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-muted-foreground file:pt-1 focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 border-border focus-visible:ring-ring placeholder:text-muted-foreground'
+          class: "flex h-9 w-full rounded-md border bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-muted-foreground file:pt-1 focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 border-border focus-visible:ring-ring placeholder:text-muted-foreground"
         }
       end
     end
